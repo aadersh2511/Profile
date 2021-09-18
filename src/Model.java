00d02c95a3db361a632e2d8672c6aa9bdc21bc52
a475393 <Aman.Aadersh@team.neustar>
2021-09-18 12:39:02 +0530

    first commit in branch1

---
+++ src/Model.java
@@ -0,0 +1,34 @@
+public class Model {
+    private String name;
+    private Integer id;
+    private String address;
+    public Model(String n,Integer id,String address){
+        this.name=n;
+        this.id=id;
+        this.address=address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
